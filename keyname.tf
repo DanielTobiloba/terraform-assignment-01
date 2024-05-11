8c949c1bbd5c23ae760656d98ef2b01226a83b63
DanielTobiloba <164507504+DanielTobiloba@users.noreply.github.com>
2024-05-11 16:46:16 +0100

    Update keyname.tf

--- keyname.tf
+++ keyname.tf
@@ -1,5 +1,5 @@
 # Defining Key Name for connection
 variable "keyname" {
-  default     = "terraform-project" 
-  description = "DANIELCLOUDCOOSTAKEY"
+  default     = "DANIELCLOUDCOOSTAKEY" 
+  description = "anything"
 }
